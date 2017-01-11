4dd9b27eeefbc0848d4e5a030299920a8f4508d7
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-01-11 14:54:08 -0500

    adding cell line metadata into column names, working on building pandas dataframe with tuple metadata

--- notebook/process_CCLE.py
+++ notebook/process_CCLE.py
@@ -1,9 +1,55 @@
 def main():
 
+  # # run once: save metadata to json
+  # get_cell_line_metadata()
+
   proc_CCLE()
 
+def get_cell_line_metadata():
+
+  import json_scripts
+
+  f = open('../original_data/CCLE_sample_info_file_2012-10-18.txt', 'r')
+  lines = f.readlines()
+  f.close()
+
+  cl_meta = {}
+
+  for i in range(len(lines)):
+
+    # skip title row
+    if i > 0:
+
+      inst_line = lines[i].strip().split('\t')
+
+      ccle_name = inst_line[0]
+
+      inst_info = {}
+      inst_info['primary_name'] = inst_line[1]
+      inst_info['gender'] = inst_line[3]
+      inst_info['tissue'] = inst_line[4]
+      inst_info['hist'] = inst_line[5]
+      inst_info['hist_sub'] = inst_line[6]
+      inst_info['notes'] = inst_line[7]
+      inst_info['source'] = inst_line[8]
+      inst_info['exp_array'] = inst_line[9]
+
+      if i == 1:
+        print(inst_info)
+        print('\n')
+
+      # save to cl_meta
+      cl_meta[ccle_name] = inst_info
+
+  json_scripts.save_to_json(cl_meta, '../proc_data/CCLE_CL_meta.json', indent='indent')
+
+  all_keys = cl_meta.keys()
+
+
 def proc_CCLE():
-  print('processing CCLE from initial GCT file')
+  import json_scripts
+
+  cl_meta = json_scripts.load_to_dict('../proc_data/CCLE_CL_meta.json')
 
   # open file
   filename = '../original_data/CCLE_Expression_Entrez_2012-09-29.gct'
@@ -28,24 +74,42 @@ def proc_CCLE():
   row_names = []
   col_names = []
 
-  # make tuple cell line names
+  # add meta-data to cell lines
+  # tissue, histology, sub-histology, gender
+  for inst_cl in cell_lines_long:
+
+    cell_line_short_name = inst_cl.split('_')[0]
+    inst_tuple = (cell_line_short_name,)
+
+    inst_tuple = inst_tuple + (cl_meta[inst_cl]['tissue'],)
+    inst_tuple = inst_tuple + (cl_meta[inst_cl]['hist'],)
+    inst_tuple = inst_tuple + (cl_meta[inst_cl]['hist_sub'],)
+
+    inst_gender = cl_meta[inst_cl]['gender']
+    if inst_gender == '':
+      inst_gender = 'NA'
+
+    inst_tuple = inst_tuple + (inst_gender,)
+
+    col_names.append(inst_tuple)
 
 
-  # loop through the file, save gene names and values
-  for i in range(len(ccle_ini)):
 
-    # skip the first two lines
-    if i > 2:
+  # # loop through the file, save gene names and values
+  # for i in range(len(ccle_ini)):
 
-      if i % 1000 == 0:
-        print('i\t' + str(i))
+  #   # skip the first two lines
+  #   if i > 2:
 
-      # get row
-      inst_row = ccle_ini[i].split('\t')
+  #     if i % 1000 == 0:
+  #       print('i\t' + str(i))
 
-      # # make sure that there is a gene name: check the first letter
-      # if str.isalpha(inst_row[0]):
+  #     # get row
+  #     inst_row = ccle_ini[i].split('\t')
 
-      #   pass
+  #     # # make sure that there is a gene name: check the first letter
+  #     # if str.isalpha(inst_row[0]):
+
+  #     #   pass
 
 main()

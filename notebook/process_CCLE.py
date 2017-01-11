a175842aa61a3d178f57d7ff7663b6125b2c28a1
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-01-11 14:38:26 -0500

    working on bringing in cell-line metadata

--- notebook/process_CCLE.py
+++ notebook/process_CCLE.py
@@ -11,8 +11,41 @@ def proc_CCLE():
   ccle_ini = f.readlines()
   f.close()
 
-  print(len(ccle_ini))
+  # get all cell lines
+  row_cl = 2
+  cell_lines_long = ccle_ini[row_cl].split()[2:]
+
+  cell_lines = [d.split('_')[0] for d in cell_lines_long]
+
+  # gene symbols are on the second column
+  row_gs = 1
+  # the data starts on the third column
+  data_start = 2
+
+  # number of cell lines
+  num_cl = len(cell_lines)
+
+  row_names = []
+  col_names = []
+
+  # make tuple cell line names
 
 
+  # loop through the file, save gene names and values
+  for i in range(len(ccle_ini)):
+
+    # skip the first two lines
+    if i > 2:
+
+      if i % 1000 == 0:
+        print('i\t' + str(i))
+
+      # get row
+      inst_row = ccle_ini[i].split('\t')
+
+      # # make sure that there is a gene name: check the first letter
+      # if str.isalpha(inst_row[0]):
+
+      #   pass
 
 main()

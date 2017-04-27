0d58601d8d45e42c39c78808e83701648d5e282c
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-04-27 12:51:56 -0400

    working on simplified page that will display single heatmap

--- js/load_multiple_clustergrams.js
+++ js/load_multiple_clustergrams.js
@@ -7,33 +7,9 @@ resize_container();
 var hzome = ini_hzome();
 
 default_args = {};
-  default_args.row_tip_callback = hzome.gene_info;
-  default_args.matrix_update_callback = matrix_update_callback;
-  default_args.dendro_callback = dendro_callback;
-
-function make_clust(make_sim_mats){
-  var clust_name = 'intra-norm_bone.json'
-
-  d3.json('json/'+clust_name, function(network_data){
-
-    var args = $.extend(true, {}, default_args);
-
-    args.root = '#container-id-1';
-    args.network_data = network_data;
-
-    cgm['clust'] = Clustergrammer(args);
-    d3.select(cgm['clust'].params.root+' .wait_message').remove();
-    cat_colors = cgm['clust'].params.viz.cat_colors;
-
-    check_setup_enrichr(cgm['clust']);
-
-    make_sim_mats('col', cat_colors);
-    make_sim_mats('row', cat_colors);
-
-  });
-
-}
-
+default_args.row_tip_callback = hzome.gene_info;
+default_args.matrix_update_callback = matrix_update_callback;
+default_args.dendro_callback = dendro_callback;
 
 d3.select('.blockMsg').select('h1').text('Please wait...');
 
@@ -43,7 +19,7 @@ $(document).ready(function(){
     $(this).scrollTop(0);
 });
 
-make_clust(make_sim_mats)
+make_heatmaps('haematopoietic_and_lymphoid_tissue');
 
 d3.select(window).on('resize',function(){
   resize_container();
@@ -62,62 +38,25 @@ window.onscroll = function() {
   var hide_col_sim = 1800;
   var inst_scroll = $(document).scrollTop();
 
-  // // load col sim mat
-  // if (inst_scroll > show_col_sim){
-  //   if (d3.select('#container-id-2 .viz_svg').empty()){
-  //     make_sim_mats('col', cat_colors)
-  //   }
-  // }
-
-  // // load row sim mat
-  // if (inst_scroll > show_row_sim){
-  //   if (d3.select('#container-id-3 .viz_svg').empty()){
-  //     make_sim_mats('row', cat_colors)
-  //   }
-  // }
-
-  // hide clust
-  if (inst_scroll > hide_clust){
-    d3.select('#container-id-1 .viz_svg')
-      .style('display', 'none');
-  } else {
-    d3.select('#container-id-1 .viz_svg')
-      .style('display', 'block');
-  }
-
-  // hide col sim mat
-  if (inst_scroll > hide_col_sim || inst_scroll < show_col_sim){
-    d3.select('#container-id-2 .viz_svg')
-      .style('display', 'none');
-  } else {
-    d3.select('#container-id-2 .viz_svg')
-      .style('display', 'block');
-  }
-
 }
 
-function make_sim_mats(inst_rc, cat_colors){
+function make_heatmaps(inst_type){
 
-  clust_name = 'mult_view_sim_'+inst_rc+'.json';
-  d3.json('json/'+clust_name, function(network_data){
+  clust_name = 'json/intra-norm_'+ inst_type + '.json';
+
+  d3.json(clust_name, function(network_data){
 
     var args = $.extend(true, {}, default_args);
     args.cat_colors = {};
-    if (inst_rc === 'col'){
-      tmp_num = 2;
-      args.cat_colors.row = cat_colors.col;
-      args.cat_colors.col = cat_colors.col;
-    } else if (inst_rc === 'row'){
-      tmp_num = 3;
-      args.cat_colors.row = cat_colors.row;
-      args.cat_colors.col = cat_colors.row;
-    }
+
+    // always place in section 2
+    tmp_num = 2;
 
     args.root = '#container-id-'+tmp_num;
 
     args.network_data = network_data;
-    cgm[inst_rc] = Clustergrammer(args);
-    d3.select(cgm[inst_rc].params.root+' .wait_message').remove();
+    cgm[inst_type] = Clustergrammer(args);
+    d3.select(cgm[inst_type].params.root+' .wait_message').remove();
   });
 
 }
